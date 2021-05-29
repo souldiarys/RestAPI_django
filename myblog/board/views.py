60cea1cae127c375dd754d79d282df7fae6adc2a
souldiarys <souldiarys@naver.com>
2021-05-29 11:37:00 +0900

    [CH3] REST API를 이용한 Django 게시판 만들기 - 페이징 기능 추가

--- myblog/board/views.py
+++ myblog/board/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+from .models import Post
 
 def post_list(request):
-    context = {}
+    post_list = Post.objects.all()
+
+    page = request.GET.get('page', '1')
+    paginator = Paginator(post_list, 2)
+    page_obj = paginator.get_page(page)
+
+    context = {'post_list': page_obj, 'page': page}
     return render(request, 'post_list.html', context)
