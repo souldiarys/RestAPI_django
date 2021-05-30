849f4fba7f9a31f3e73308fdd159f1b7db714ecd
souldiarys <souldiarys@naver.com>
2021-05-30 20:55:50 +0900

    [CH4] REST API를 이용한 Django 게시판 만들기 - 로그인 기능 추가

--- myblog/board/urls.py
+++ myblog/board/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path, include
+from django.urls import path
 
 from . import views
 
 app_name = 'board'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('login/', views.login, name='login'),
+    path('login/validate', views.login_validate, name='login_validate'),
 ]
